b1db6773fb4beb88adbcce39b028d97ca6380e16
Rohit Kumar Sah <rohitsah710@gmail.com>
2021-08-29 11:17:28 +0530

    creating custom component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    template: `
+                <div class="card">
+                    <div class="card-body">
+                        Hi from AppServer Component
+                    </div>
+                </div>
+            `,
+    styles: []
+})
+export class ServerComponent { }
