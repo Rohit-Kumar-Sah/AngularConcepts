5db2b698ea859f8e052c69ef79c98b2336b58dba
Rohit Kumar Sah <rohitsah710@gmail.com>
2021-08-31 23:29:52 +0530

    View Encapsulation

--- src/app/cockpit-display/cockpit-display.component.ts
+++ src/app/cockpit-display/cockpit-display.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit-display',
   templateUrl: './cockpit-display.component.html',
   styleUrls: ['./cockpit-display.component.css']
+  // , encapsulation: ViewEncapsulation.Emulated ; this is default value that helps preserve style of a component to itself only
 })
 export class CockpitDisplayComponent implements OnInit {
   @Input('childPit') cpit: { name: string, des: string, color: string };
