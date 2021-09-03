11fe7dea94b05b9230ce43787fc8f2badf3cd236
Rohit Kumar Sah <rohitsah710@gmail.com>
2021-09-03 20:37:13 +0530

    ViewChild in action

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,15 +9,23 @@ export class CockpitComponent implements OnInit {
 
   @Output('sf') myserverform = new EventEmitter<{ name: string, des: string, color: string }>();
   @Output('bf') myblueprintform = new EventEmitter<{ name: string, des: string, color: string }>();
+
+  @ViewChild('sname') sname1;
+  @ViewChild('sdes') sdes1;
+
   constructor() { }
 
   ngOnInit(): void {
   }
   AddServer(sname, sdes) {
+    console.log("Printing via viewChild", this.sname1.nativeElement.value)
+    console.log("Printing via viewChild", this.sdes1.nativeElement.value)
     this.myserverform.emit({ name: sname.value, des: sdes.value, color: "red" });
     //fetched value of local reference
   }
   AddBlueprint(sname, sdes) {
+    console.log("Printing via viewChild", this.sname1.nativeElement.value)
+    console.log("Printing via viewChild", this.sdes1.nativeElement.value)
     this.myserverform.emit({ name: sname.value, des: sdes.value, color: "blue" });
     //fetched value of local reference
 
