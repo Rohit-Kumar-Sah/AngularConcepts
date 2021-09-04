5e4915bf6b78a9c6c3f778ffd5135a7b0c663151
Rohit Kumar Sah <rohitsah710@gmail.com>
2021-09-04 12:28:49 +0530

    Renderer2 in action : (elegant way to work with Dom)

--- src/app/shared/basicHighter.directive.ts
+++ src/app/shared/basicHighter.directive.ts
@@ -1,14 +1,20 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[appBasicHighlight]' //we wish to use this directive as attribute on element 
 })
 export class basicHighlightDirective implements OnInit {
-    constructor(private theElement: ElementRef) { //angular gives us the element on which directive will sit
+    /* constructor(private theElement: ElementRef) { //angular gives us the element on which directive will sit
+     }
+     ngOnInit() {
+         this.theElement.nativeElement.style.color = "salmon";
+     }
+    */
 
-    }
+    constructor(private renderer: Renderer2, private theElement: ElementRef) { }
 
     ngOnInit() {
-        this.theElement.nativeElement.style.color = "salmon";
+        this.renderer.setStyle(this.theElement.nativeElement, 'color', 'salmon');
+        //(element,property,value,flags such as !important) last parameter is optional
     }
 }
