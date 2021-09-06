7bc8b916cc704ca01a6735d4da8b054e5f8e1ddb
Rohit Kumar Sah <rohitsah710@gmail.com>
2021-09-07 00:33:48 +0530

    prokect building around route, route params , fragment

--- src/app/companies/single-company/single-company.component.ts
+++ src/app/companies/single-company/single-company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -9,11 +10,24 @@ import { ServersService } from '../servers.service';
 export class SingleCompanyComponent implements OnInit {
 
   server: { id: number, name: string, status: string };
+  hereSubscription: any;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    //this.server = this.serversService.getServer(1);
+
+    /*
+    //fetching route parameters //these works once only when the page url is loaded from some other page;
+    //just incase url gets updated from WITHIN the page the page then this snapshot wouldnt be able to catch it and update the view
+    let urlid = this.activatedRoute.snapshot.params["id"];
+    this.server = this.serversService.getServer(+(urlid));
+    */
+
+    //Reactively fetching route parameters
+    this.hereSubscription = this.activatedRoute.params.subscribe((data: Params) => {
+      this.server = this.serversService.getServer(+(data.id));
+    })
   }
 
 }
