62caaf0bed54a032774c75d783ece0c1e0915437
Rohit Kumar Sah <rohitsah710@gmail.com>
2021-09-06 23:57:16 +0530

    Retrieving Query Parameters and Fragments

--- src/app/companies/edit-companies/edit-companies.component.ts
+++ src/app/companies/edit-companies/edit-companies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -12,12 +13,44 @@ export class EditCompaniesComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+
+    /*
+    //fetching route queryParams //these works once only when the page url is loaded from some other page;
+    //just incase url gets updated from WITHIN the page the page then this snapshot wouldnt be able to catch it and update the view
+    let urlid = this.activatedRoute.snapshot.queryParams["id"];
+    let urlname = this.activatedRoute.snapshot.queryParams["name"];
+    this.user = {
+      id: urlid,
+      name: urlname
+    }
+    
+
+    //Reactively fetching route parameters
+   this.activatedRoute.queryParams.subscribe((data) => {
+      
+    })
+    */
+
+    /*
+    //fetching route fragment //these works once only when the page url is loaded from some other page;
+    //just incase url gets updated from WITHIN the page the page then this snapshot wouldnt be able to catch it and update the view
+    
+    let allowEdit = this.activatedRoute.snapshot.fragment;
+    
+
+    //Reactively fetching route parameters
+   this.activatedRoute.fragment.subscribe((data) => {
+      
+    })
+    */
+
+
   }
 
   onUpdateServer() {
