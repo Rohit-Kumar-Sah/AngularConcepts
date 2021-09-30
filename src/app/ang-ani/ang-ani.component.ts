59e22eca03369a6a428ee64beb56e0c8772a8135
Rohit Kumar Sah <rohitsah710@gmail.com>
2021-09-30 10:17:25 +0530

    having synchronous intermediary phases

--- src/app/ang-ani/ang-ani.component.ts
+++ src/app/ang-ani/ang-ani.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
       'myelement',
       [
         state('normal', style({ backgroundColor: 'green', transform: 'translateX(0px)' })),
-        state('highlighted', style({ backgroundColor: 'red', transform: 'translateX(100px)' })),
+        state('highlighted', style({ backgroundColor: 'red', transform: 'translateX(1000px)' })),
         /*transition('normal => highlighted', animate(200)),
         transition('highlighted => normal', animate(200)),   these 2 line replaced by below line*/
-        transition('normal <=> highlighted', animate(200))
+        //transition('normal <=> highlighted', animate(2000)),
+        transition('normal<=>highlighted', [
+          group([
+
+            animate(1000, style({ borderRadius: '50%' })), //normal state to changing border over 1sec
+            // animate(3000, style({ transform: 'translateX(500px)' })), //with changed border to moving halfway over another 1sec
+            animate(1000, style({ backgroundColor: 'gray' })), //at halfway + changed border to gray background over another 1sec
+            animate(1000)                                       //at halfway + changed border + gray background to highlighted state over another 1sec
+          ])
+        ])
       ],
     )
   ]
